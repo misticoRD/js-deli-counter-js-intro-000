fb8dc194975db7e954d46cf82d96cd4b71b699dc
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 20:58:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function  currentLine (katzDeliLine) {
   if (katzDeliLine.length===0) {
   return "The line is currently empty.";
 }
-   let customerNum = [];
+   let customerNumber = [];
    for (var i = 0; i<katzDeliLine.length; i++) {
    customerNumber.push(i+1 + ". " + katzDeliLine[i])
 }
