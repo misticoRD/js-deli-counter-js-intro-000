c41d34ec3eb2f02d7e5b93bb492acd335a49c816
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-10 20:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,3 @@ name = deliLine.shift();
 return `Currently serving ${name}.`
 }
 
-function currentLine(line) {
-  let i = 0;
-  while (i < line.length) {
-    line.push(`${[i+1]}. ${line[i]}`)
-    i++;
-  }
-  if (line.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently: ${line}`);
-}
